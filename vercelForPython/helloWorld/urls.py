e06acd8d30a73a4a7331494ef7146c22d8d1df1d
macman1717 <mchawk1717@gmail.com>
2025-02-07 14:55:46 -0500

    testing db hosting

--- vercelForPython/helloWorld/urls.py
+++ vercelForPython/helloWorld/urls.py
@@ -5,4 +5,6 @@ app_name = "helloWorld"
 
 urlpatterns = [
     path("", views.hello_world, name="hello"),
+    path("post_blog/<str:name>", views.save, name="hello"),
+    path("get_blog/<str:name>", views.get_blog, name="get_name"),
 ]
--- vercelForPython/helloWorld/views.py
+++ vercelForPython/helloWorld/views.py
@@ -1,6 +1,17 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from django.shortcuts import render, get_object_or_404
+
+from .models import Blog
+
 
 # Create your views here.
 def hello_world(request):
     return HttpResponse("Hello World!")
+
+def save(request, name):
+    blog = Blog(name=name , tagline="My name is " + name)
+    blog.save()
+
+def get_blog(request, name):
+    blog = Blog.objects.filter(name=name).values("name", "tagline").first()
+    return JsonResponse({"blog":blog})
